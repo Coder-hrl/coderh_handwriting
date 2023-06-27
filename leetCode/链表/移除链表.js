57658ea05194464051c7efb99e79b5a83208843a
huangruilin <1310145384@qq.com>
2023-06-27 19:59:25 +0800

    init:代码二次初始话

--- leetCode/链表/移除链表.js
+++ leetCode/链表/移除链表.js
@@ -4,6 +4,7 @@ var removeListNode = (head, val) => {
   return head.val === val ? head.next : head
 }
 
+// 给好指定的点,使得新生成的列表是移除这个点
 var removeListNode = (head, val) => {
   let el = {
     next: head,
